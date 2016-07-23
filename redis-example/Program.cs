eb3e0300c74659c97c45ee934a8d1eb1f8d1a79a
tonykung <tonykung06@gmail.com>
2016-07-23 20:42:37 +0800

    using IRedisNativeClient abstraction to interface with Redis server

---
+++ redis-example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+
+namespace redis_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            usingLowLevelInterface();
+        }
+
+        //IRedisNativeClient has one-to-one mapping to Redis official APIs
+        static void usingLowLevelInterface()
+        {
+            using (IRedisNativeClient client = new RedisClient())
+            {
+                client.Set("urn:messages:1", Encoding.UTF8.GetBytes("Hello C# World"));
+            }
+            using (IRedisNativeClient client = new RedisClient())
+            {
+                var result = Encoding.UTF8.GetString(client.Get("urn:messages:1"));
+                Console.WriteLine("Message: {0}", result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
